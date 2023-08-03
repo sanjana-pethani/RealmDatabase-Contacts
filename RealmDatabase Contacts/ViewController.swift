01c2d077436c827f977cf9a4c2d4e0dec5b30f4a
Sanjana <sanjanapethani@gmail.com>
2023-08-03 09:56:27 +0530

    Initial Commit

---
+++ RealmDatabase Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmDatabase Contacts
+//
+//  Created by sanjana pethani on 03/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
